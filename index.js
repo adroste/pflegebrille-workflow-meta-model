0a00d747225267ad8a7fdf761a2d7d2f21276cbc
Alexander Droste <mail@adroste.com>
2021-01-03 16:17:10 +0100

    fix: remove undefined exports

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import { DataTypeEnum, DataTypeLabels } from './enum/DataTypeEnum';
-import { ElementPositionEnum, ElementPositionLabels } from './enum/ElementPositionEnum';
-
+import { DataTypeEnum } from './enum/DataTypeEnum';
+import { ElementPositionEnum } from './enum/ElementPositionEnum';
 import { pbModdle } from './moddle';
 
 export default pbModdle;
 export { 
     pbModdle,
     DataTypeEnum,
-    DataTypeLabels,
     ElementPositionEnum,
-    ElementPositionLabels,
 };
