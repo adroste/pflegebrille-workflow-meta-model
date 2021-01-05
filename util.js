ede41c55e6b7d5345bde167947ba27dd88db9fee
Alexander Droste <mail@adroste.com>
2021-01-05 12:05:16 +0100

    feat: utils - uriRegex, makeUri helper

--- util.js
+++ util.js
@@ -1,3 +1,9 @@
+export const uriRegex = /(http:\/\/pflegebrille.de\/schema\/bpmn\/pb-extension\/v)([^"']+)($|"|')/;
+
+export function makeUri(version) {
+    return 'http://pflegebrille.de/schema/bpmn/pb-extension/v' + version;
+}
+
 export function enumToModdleEnum(name, enumType) {
     const moddleEnum = {
         name,
