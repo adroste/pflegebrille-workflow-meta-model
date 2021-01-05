c3dbe8c2aec8a29a2653b274c9be43a09daf39fb
Alexander Droste <mail@adroste.com>
2021-01-05 12:25:11 +0100

    fix: missing export for checkVersion

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { DataTypeEnum } from './enum/DataTypeEnum';
 import { ElementPositionEnum } from './enum/ElementPositionEnum';
+import { checkVersion } from './migrate';
 import v1 from './model/v1';
 
 export default v1; // latest
 
 export { 
     v1,
+    checkVersion,
     DataTypeEnum,
     ElementPositionEnum,
 };
